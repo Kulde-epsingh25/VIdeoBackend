5c5afb2646b2bf6bdc19f3159e0c67fc70d167bf
Kuldeep-singh25 <kuldeepsingh12311087@gmail.com>
2026-09-18 17:16:53 +0530

    add user password management and profile update features

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -205,6 +205,92 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 });
 
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+    const {currentPassword, newPassword , confirmNewPassword} = req.body;
+
+    const user = await User.findById(req.user._id);
+    const isPasswordCorrect = await user.isPasswordCorrect(currentPassword);
+    if(!isPasswordCorrect){
+        throw new ApiError(401, "Current password is incorrect");
+    }
+    if(newPassword !== confirmNewPassword){
+        throw new ApiError(400, "New password and confirm new password do not match");
+    }
+    user.password = newPassword;
+    await user.save({validateBeforeSave: false}); // save the new password to the database without validating other fields
+    return res
+    .status(200)
+    .json(new ApiResponse(200, null, "Password changed successfully"));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully"));
+});
+
+const updateCurrentUser = asyncHandler(async (req, res) => {
+    const {fullName, username, email} = req.body;
+   
+    if(!fullName || !email){
+        throw new ApiError(400, "Full name and email are required");
+    }
+    
+    const user = await UserfindbyIdAndUpdate(req.user._id,
+        {
+            $set: {
+                fullName,
+                username: username?.toLowerCase(),
+                email: email?.toLowerCase()
+            }
+        }, {new: true}
+    ).select("-password -refreshToken"); // remove password and refreshToken from response
+    await user.save({validateBeforeSave: false});
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully"));
+});
+
+const updateCurrentUserAvatar = asyncHandler(async (req, res) => {
+    const avatarLocalPath =  req.file?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is missing");
+    }
+    const avatar = await uploadToCloudinary(avatarLocalPath);
+    if(!avatar.url){
+        throw new ApiError(500, "Failed to upload avatar");
+    }
+    const user = await User.findByIdAndUpdate(req.user._id,
+        {
+            $set: {avatar: avatar.url}
+        }, {new: true}
+    ).select("-password -refreshToken");
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar updated successfully"));
+});
+
+const updateCurrentUserCoverImage = asyncHandler(async (req, res) => {
+    const coverImageLocalPath =  req.file?.path;
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "Cover image file is missing");
+    }
+    const coverImage = await uploadToCloudinary(coverImageLocalPath);
+    if(!coverImage.url){
+        throw new ApiError(500, "Failed to upload cover image");
+    }
+    const user = await User.findByIdAndUpdate(req.user._id,
+        {
+            $set: {coverImage: coverImage.url}
+        }, {new: true}
+    ).select("-password -refreshToken");
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover image updated successfully"));
+});
 
 
-export {registerUser , loginUser , refreshAccessToken , logoutUser};
+
+
+export {registerUser , loginUser , refreshAccessToken , changeCurrentPassword , getCurrentUser, updateCurrentUser, logoutUser, updateCurrentUserAvatar, updateCurrentUserCoverImage};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import { registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { loginUser, logoutUser } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { refreshAccessToken } from "../controllers/user.controller.js";
 const router = Router();
 
 router.post('/register',upload.fields([
