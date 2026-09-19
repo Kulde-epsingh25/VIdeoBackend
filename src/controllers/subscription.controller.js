7e4d72d477ce5ef92038e32e4b43cd1085b1b6c3
Kuldeep-singh25 <kuldeepsingh12311087@gmail.com>
2026-09-19 22:04:58 +0530

    implement subscription toggle functionality; add validation and response handling for channel subscriptions

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,16 +9,72 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    // check if the channelId is valid 
+    // check if the channelId is not same as the userId of the logged in user
+    // check if the channelId exists in the User database
+    // find channelId in the Subscription collection with subscriber as logged in user and channel as channelId
+    // toggle the subscription status, if it exists then delete it, if it doesn't exist then create a new subscription document
+    // return the response with status 200 and the subscription status (subscribed or unsubscribed)
+
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channelId");
+    }; //isValidObjectId is a mongoose method to check if the id is valid or not 
+
+    if(channelId === req.user._id.toString()) {
+        throw new ApiError(400, "You cannot subscribe to yourself");
+    };
+
+    const channel = await User.findById(channelId);
+    if (!channel) {
+        throw new ApiError(404, "Channel not found");
+    };
+
+    const existingSubscription = await Subscription.findOne({
+        subscriber: req.user._id,
+        channel: channelId
+    });
+
+    if (existingSubscription) {
+        await existingSubscription.deleteOne();
+        return res.status(200).json(new ApiResponse(200, null, "Unsubscribed successfully"));
+    } else {
+        const newSubscription = await Subscription.create({
+            subscriber: req.user._id,
+            channel: channelId
+        });
+        if (!newSubscription) {
+            throw new ApiError(500, "Failed to subscribe");
+        };
+        return res.status(200).json(new ApiResponse(200, newSubscription, "Subscribed successfully"));
+    }
+
+
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Invalid channelId");
+    }
+    const subscribers = await Subscription.find({ channel: channelId });
+    if (!subscribers) {
+        throw new ApiError(404, "No subscribers found for this channel");
+    }
+    return res.status(200).json(new ApiResponse(200, subscribers, "Subscribers fetched successfully"));
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Invalid subscriberId");
+    }
+    const subscribedChannels = await Subscription.find({ subscriber: subscriberId });
+    if (!subscribedChannels) {
+        throw new ApiError(404, "No subscribed channels found for this user");
+    }
+    return res.status(200).json(new ApiResponse(200, subscribedChannels, "Subscribed channels fetched successfully"));
 })
 
 export {
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -14,7 +14,26 @@ const subscriptionSchema = new Schema({
             timestamps: true
         });
 
+// Subscriber: The user who is subscribing to the channel
+// Channel: The user whose channel is being subscribed to
+// both are user objects 
+// user(subscriber) : a, b, c, d, e
+// channel : P, Q, R, e
 
+// a -> P, Q, R so for each subscription new document will be created in subscription collection 
+// { subscriber: a, channel: P } , { subscriber: a, channel: Q } , { subscriber: a, channel: R }
+
+
+// b -> P, Q, e
+// { subscriber: b, channel: P } , { subscriber: b, channel: Q }, { subscriber: b, channel: e }
+
+//if we need to find 
+
+//Subscriber list of a channel P,  
+//lookup(channel: P) => { subscriber: a, channel: P }, { subscriber: b, channel: P } => a, b
+
+//Channel list of user a
+//lookup(subscriber: a) => { subscriber: a, channel: P }, { subscriber: a, channel: Q }, { subscriber: a, channel: R } => P, Q, R
 
 
 
